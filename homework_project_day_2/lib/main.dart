ddc9739dfdab1dbf27e7782bace26f637dc41450
Timofey Leonov <timofey.leonov007@gmail.com>
2021-03-17 01:09:02 +0100

    Expirienced with app

--- homework_project_day_2/lib/main.dart
+++ homework_project_day_2/lib/main.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('build/images/IMG_1380.jpg'),
+              ),
+              Text(
+                'Timofey',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'I will build an app which will record your info about the day',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
